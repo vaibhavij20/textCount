20eea4aa3d2ce7cc0afb026468767da1f9baf1e7
Vaibhavi Jaiswal <vaibhavi.jaiswal2023@vitstudent.ac.in>
2025-03-31 16:33:17 +0530

    Initial commit - Added Navbar component with dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 
+import { useState } from 'react';
 import './App.css';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() 
 {
+  const [mode,setMode]=useState("light");
+  const [t,sett]=useState("Enable dark mode")
+
+  const toggleMode=()=>{
+    if(mode==="light")
+    {
+      setMode("dark");
+      sett("Enable light mode")
+      document.body.style.backgroundColor="#454545"
+       document.body.style.color="white"
+      
+    }
+    else{
+      setMode("light")
+      sett("Enable dark mode")
+       document.body.style.backgroundColor="white"
+       document.body.style.color="black"
+      
+    }
+  }
   return (
     <>
     
-<Navbar title="TextCount" abouttext="About us"/>
+<Navbar title="TextCount" abouttext="About us" mode={mode} toggleMode={toggleMode} t={t}/>
 <div className="container my-3" >
 <TextForm heading="Enter you text here"/>
+{/*<About/>*/}
 </div>
 
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,6 +12,11 @@ export default function TextForm(props) {
     
 
   };
+  const handleLowClick=()=>{
+    let newt=text.toLowerCase();
+    setText(newt);
+
+  };
   const handleOnChange=(e)=>
     {
       
@@ -19,7 +24,23 @@ export default function TextForm(props) {
       
   
     };
-  const [text, setText] = useState('Enter text here');
+    const handleClear=()=>
+      {
+        setText("");
+      }
+
+    const handleCopy=()=>{
+      let text=document.getElementById("exampleFormControlTextarea1");
+      text.select();
+      
+      navigator.clipboard.writeText(text.value)
+    }
+    const handleExtraSpaces=()=>
+    {
+      let newtext=text.split(/[ ]+/);
+      setText(newtext.join(" "));
+    }
+  const [text, setText] = useState('');
   //setText("hey");
   return (
     <>
@@ -29,11 +50,18 @@ export default function TextForm(props) {
         <label htmlFor="exampleFormControlTextarea1" className="form-label"></label>
         <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className="btn btn-primary " onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className="btn btn-primary mx-3" onClick={handleLowClick}>Convert to LowerCase</button>
+      <button className="btn btn-primary mx-3" onClick={handleClear}>Clear</button>
+      <button className="btn btn-primary mx-3" onClick={handleCopy}>Copy</button>
+      <button className="btn btn-primary mx-3" onClick={handleExtraSpaces}>Extra Space Remover</button>
     </div>
     <div className='container my-3'>
       <h1>Your text summary</h1>
       <p>{(text.split(" ").length) } words and {text.length} characters</p>
+      <p>{0.008 *(text.split(" ").length) } minutes</p>
+      <h1>Preview</h1>
+      <p>{text.length>0?text:"Enter something to preview here"}</p>
     </div>
     </>
   )
